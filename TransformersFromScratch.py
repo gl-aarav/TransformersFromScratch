9ac3b5e8fc5069bfb2ff68c000591c53b4fe9d0a
gl-aarav <gl.aarav@gmail.com>
2025-09-01 17:13:03 -0700

    fix: correct assertion logic in MultiMHeadAttentionBlock and add attention method

--- TransformersFromScratch.py
+++ TransformersFromScratch.py
@@ -63,7 +63,7 @@ class MultiMHeadAttentionBlock(nn.Module):
         super().__init__()
         self.d_model = d_model
         self.h = h
-        assert d_model % h == 0, "d_model is not divisible by h"
+        assert d_model % h != 0, "d_model is not divisible by h"
         
         self.d_k = d_model // h
         self.w_q = nn.Linear(d_model, d_model) # Wq
@@ -72,7 +72,21 @@ class MultiMHeadAttentionBlock(nn.Module):
         
         self.w_o = nn.Linear(d_model, d_model) #Wo
         self.dropout = nn.Dropout(dropout)
+
+    @staticmethod
+    def attention(query, key, value, mask, dropout: nn.Dropout):
+        d_k = query.shape[-1]
+
+        # (Batch, h, seq_len, d_k) --> (Batch, h, seq_len)
+        attention_scores = (query @ key.transpose(-2, -1)) / math.sprt(d_k)
+        if mask is not None:
+            attention_scores.masked_fill_(mask == 0, -1e9)
+        attention_scores = attention_scores.softmax(dim=-1) # (Batch, h, seq_len, seq_len)
+        if dropout is not None:
+            attention_scores = dropout(attention_scores)
         
+        return (attention_scores @ value), attention_scores
+
     def forward(self, q, k, v, mask):
         query = self.w_q(q) # (Batch, seq_len, d_model) --> (Batch, seq_lean, d_model)
         key = self.w_k(k) # (Batch, seq_len, d_model) --> (Batch, seq_lean, d_model)
@@ -81,26 +95,31 @@ class MultiMHeadAttentionBlock(nn.Module):
         # (Batch, seq_len, d_model) --> (Batch, seq_len, h, d_k) --> (Batch, h, seq_len, d_k)
         query = query.view(query.shape[0], query.shape[1], self.h, self.d_k).transpose(1, 2)
         key = query.view(key.shape[0], key.shape[1], self.h, self.d_k). transpose(1, 2)
-        value = value.view(value.shape[0], value.shape[1], shape.head)
+        value = value.view(value.shape[0], value.shape[1], self.h, self.d_k).transpose(1,2)
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+        x, self.attention_scores = MultiMHeadAttentionBlock.attention(query, key, value, mask, self.dropout)
+
+        # (Batch, h, seq_len, d_k) --> (Batch, seq_len, h, d_k) --> (Batch, seq_len, d_model)
+        x = x.transpose(1,2).contiguous().view(x.shape[0], -1, self.h * self.d_k)
+
+        # (Batch, seq_len, d_model) --> (Batch, seq_len, d_model)
+        return self.w_o(x)
+    
+class ResidiualConnection(nn.Module):
+    def __init__(self, dropout: float) -> None:
+        super().__init__()
+        self.dropout = nn.Dropout(dropout)
+        self.norm = LayerNormalization()
+
+    def forward(self, x, sublayer):
+        return x + self.dropout(sublayer(self.norm(x)))
+    
+class EncoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiMHeadAttentionBlock, feed_forward_block, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.feed_forward_block =  feed_forward_block
+        self.residual_connections = nn.ModuleList([ResidiualConnection(dropout) for _ in range(2)])
+
+    def forward(self, x, src_mask):
+        x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
