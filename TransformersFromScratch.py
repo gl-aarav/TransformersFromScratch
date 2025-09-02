6329875803520d9b19ca6f7823c9621b06c05bb1
gl-aarav <gl.aarav@gmail.com>
2025-09-01 18:17:27 -0700

    updates

--- TransformersFromScratch.py
+++ TransformersFromScratch.py
@@ -123,3 +123,28 @@ class EncoderBlock(nn.Module):
 
     def forward(self, x, src_mask):
         x = self.residual_connections[0](x, lambda x: self.self_attention_block(x, x, x, src_mask))
+        x = self.residual_connections[1](x, self.feed_forward_block)
+        return x
+    
+class Encoder(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNormalization()
+        
+    def forward(self, x, mask):
+        for layer in self.layers:
+            x = layer(x, mask)
+        return self.norm(x)
+    
+class DecoderBlock(nn.Module):
+    def __init__(self, self_attention_block: MultiMHeadAttentionBlock, cross_attention_block: MultiMHeadAttentionBlock, feed_forward_block: FeedForwardBlock, dropout: float) -> None:
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connections = nn.Module([ResidiualConnection(dropout) for _ in range(3)])
+
+    def forward
+
+        
